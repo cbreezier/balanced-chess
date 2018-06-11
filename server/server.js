1cec5b4e67b33d8eaf6350615f82776565e67caa
Leo Huang <leo.tx.huang@gmail.com>
2018-06-11 00:13:25 -0700

    Add babel things to server

--- server/server.js
+++ server/server.js
@@ -3,11 +3,14 @@
 
 const express = require('express');
 
+import { helloWorld } from 'common';
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+  // res.send({ express: `Hello From Express: ${helloWorld()}` });
+  res.send({ express: `Hello From Express` });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
