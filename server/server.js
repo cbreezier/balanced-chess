b39bc7ec99a9227451034ef5794a297eff575957
Leo Huang <leo.tx.huang@gmail.com>
2018-06-11 00:29:07 -0700

    Add babel-preset-flow to server

--- server/server.js
+++ server/server.js
@@ -9,8 +9,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/api/hello', (req, res) => {
-  // res.send({ express: `Hello From Express: ${helloWorld()}` });
-  res.send({ express: `Hello From Express` });
+  res.send({ express: sampleResponse() });
 });
+
+function sampleResponse(): string {
+  return `Hello From Express: ${helloWorld()}`;
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
